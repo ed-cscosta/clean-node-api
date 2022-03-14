6570c898607c8c97c896271a1a051d03cf2ed08a
Eduardo <ed_cscosta@hotmail.com>
2022-03-14 20:30:34 +0000

    ensure EmailValidatorAdapter calls validator with correct email

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,10 @@
 export default {
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   preset: 'ts-jest',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,24 @@
+import { EmailValidatorAdapter } from './email-validator-adapter';
+import validator from 'validator';
+
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter();
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
+    const isValid = sut.isValid('invalid_email');
+    expect(isValid).toBe(false);
+  });
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid('valid_email@mail.com');
+    expect(isValid).toBe(true);
+  });
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter();
+    const isEmailSpy = jest.spyOn(validator, 'isEmail');
+    sut.isValid('valid_email@mail.com');
+    expect(isEmailSpy).toHaveBeenCalledWith('valid_email@mail.com');
+  });
+});
