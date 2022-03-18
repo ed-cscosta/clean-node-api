af1215be5e01cbb1f82ac9aa9c7b7d3f105f25f7
Eduardo <ed_cscosta@hotmail.com>
2022-03-18 03:05:15 +0000

    ensure DbAccount throws if Encrypter throws

--- src/data/usecases/add-account/db-create-account.spec.ts
+++ src/data/usecases/add-account/db-create-account.spec.ts
@@ -1,14 +1,30 @@
 import { DbCreateAccount } from './db-create-account';
+import { Encrypter } from './protocols/encrypter';
+
+interface SutTypes {
+  sut: DbCreateAccount;
+  encrypterStub: Encrypter;
+}
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(password: string): Promise<string> {
+      return await new Promise((resolve) => resolve('hashed_password'));
+    }
+  }
+
+  return new EncrypterStub();
+};
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter();
+  const sut = new DbCreateAccount(encrypterStub);
+  return { sut, encrypterStub };
+};
 
 describe('DbCreateAccount', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt(password: string): Promise<string> {
-        return await new Promise((resolve) => resolve('hashed_password'));
-      }
-    }
-    const encrypterStub = new EncrypterStub();
-    const sut = new DbCreateAccount(encrypterStub);
+    const { sut, encrypterStub } = makeSut();
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
     const accountData = {
       name: 'valid_name',
@@ -17,5 +33,21 @@ describe('DbCreateAccount', () => {
     };
     await sut.create(accountData);
     expect(encryptSpy).toHaveBeenCalledWith('valid_password');
+  });
+
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut();
+    jest
+      .spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    };
+    const promise = sut.create(accountData);
+    await expect(promise).rejects.toThrow();
   });
 });
